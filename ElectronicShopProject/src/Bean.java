caa9f777f400d1dee89bd300e02980170d9f7212
santhosh.gandhe <santhosh.gandhe@9f4d48c2-a7b2-11de-b195-ab392a2a73f5>
2009-10-14 11:24:39 +0000

    ElectronicsShop added

---
+++ ElectronicShopProject/src/Bean.java
@@ -0,0 +1,52 @@
+public class Bean implements Product
+{
+	String id;
+	String make;
+	double price;
+	int quantity;
+
+	public void setProductid(String productid)
+	{
+		id = productid;
+	}
+
+	public String getProductid()
+	{
+		return id;
+	}
+
+	public void setProductmake(String productmake)
+	{
+		make = productmake;
+	}
+
+	public String getProductmake()
+	{
+		return make;
+	}
+
+	public void setProductprice(double productprice)
+	{
+		price=productprice  ;
+	}
+
+	public double getProductprice()
+	{
+		return price;
+	}
+
+	public void setProductquantity(int quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public int getProductquantity()
+	{
+		return quantity;
+	}
+
+	public String toString()
+	{
+		return "\n\n\n\n id = "+id+"\n make = "+make+"\n price = "+price+" \n quantity = "+quantity;
+	}
+}
---
+++ ElectronicShopProject/src/Product.java
@@ -0,0 +1,14 @@
+public interface Product
+{
+	public void setProductid(String id);
+	public String getProductid();
+
+	public void setProductmake(String make);
+	public String getProductmake();
+	
+	public void setProductprice(double price);
+	public double getProductprice();
+
+	public void setProductquantity(int quantity);
+	public int getProductquantity();
+}
